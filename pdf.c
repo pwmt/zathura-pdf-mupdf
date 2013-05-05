d8cc41d0e2a34c9379eddfc81e1d263c7c6c10d6
Sebastian Ramacher <sebastian@ramacher.at>
2013-05-05 17:46:44 +0200

    Fix segfaults while writing
    mupdf's pdf_write_document tries to access the third argument even if it NULL
    although the documention of fz_write_document would allow a NULL argument.
    
    Closes: #307
    
    Signed-off-by: Sebastian Ramacher <sebastian@ramacher.at>

--- pdf.c
+++ pdf.c
@@ -147,7 +147,12 @@ pdf_document_save_as(zathura_document_t* document, mupdf_document_t*
   }
 
   fz_try (mupdf_document->ctx) {
-    fz_write_document(mupdf_document->document, (char*) path, NULL);
+    /* fz_write_document claims to accepts NULL as third argument but doesn't.
+     * pdf_write_document does not check if the third arguments is NULL for some
+     * options. */
+
+    fz_write_options opts = { 0 }; /* just use the default options */
+    fz_write_document(mupdf_document->document, (char*) path, &opts);
   } fz_catch (mupdf_document->ctx) {
     return ZATHURA_ERROR_UNKNOWN;
   }
