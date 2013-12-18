43f6124750f022e730d1cfe168de40d033f0b0b9
Sebastian Ramacher <sebastian@ramacher.at>
2013-12-18 16:53:48 +0100

    Fix link order
    Also only link libcrypto. Thanks to vp1981 for the patch.
    
    Closes: #379
    
    Signed-off-by: Sebastian Ramacher <sebastian@ramacher.at>

--- config.mk
+++ config.mk
@@ -29,11 +29,13 @@ ifeq (,${PLUGINDIR})
 PLUGINDIR = ${LIBDIR}/zathura
 endif
 
-OPENSSL_INC ?= $(shell pkg-config --cflags openssl)
-OPENSSL_LIB ?= $(shell pkg-config --libs openssl)
+OPENSSL_INC ?= $(shell pkg-config --cflags libcrypto)
+OPENSSL_LIB ?= $(shell pkg-config --libs libcrypto)
+
+MUPDF_LIB ?= -lmupdf -lmupdf-js-none
 
 INCS = ${GTK_INC} ${GIRARA_INC} ${OPENSSL_INC} ${ZATHURA_INC}
-LIBS = ${GTK_LIB} ${GIRARA_LIB} ${OPENSSL_LIB} -lmupdf -lmupdf-js-none -ljbig2dec -lopenjp2 -ljpeg
+LIBS = ${GTK_LIB} ${GIRARA_LIB} ${MUPDF_LIB} ${OPENSSL_LIB} -ljbig2dec -lopenjp2 -ljpeg
 
 # flags
 CFLAGS += -std=c99 -fPIC -pedantic -Wall -Wno-format-zero-length $(INCS)
