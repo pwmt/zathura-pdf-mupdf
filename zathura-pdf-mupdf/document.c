f0a62c5145f4cbb6660de994a22fa391b9b966ec
Moritz Lipp <mlq@pwmt.org>
2016-01-03 17:56:46 +0100

    Rename fz_write_document to pdf_save_document

--- zathura-pdf-mupdf/document.c
+++ zathura-pdf-mupdf/document.c
@@ -130,8 +130,8 @@ pdf_document_save_as(zathura_document_t* document, const char* path)
      * pdf_write_document does not check if the third arguments is NULL for some
      * options. */
 
-    fz_write_options opts = { 0 }; /* just use the default options */
-    fz_write_document(mupdf_document->ctx, mupdf_document->document, (char*) path, &opts);
+    pdf_write_options opts = { 0 }; /* just use the default options */
+    pdf_save_document(mupdf_document->ctx, (pdf_document*) mupdf_document->document, (char*) path, &opts);
   } fz_catch (mupdf_document->ctx) {
     return ZATHURA_ERROR_UNKNOWN;
   }
