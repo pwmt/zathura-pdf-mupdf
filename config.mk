58a4539352f68c782d9292170c8019c6540889ff
Moritz Lipp <mlq@pwmt.org>
2016-02-14 23:49:17 +0100

    Version 0.3.0

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 # See LICENSE file for license and copyright information
 
 VERSION_MAJOR = 0
-VERSION_MINOR = 2
-VERSION_REV = 9
+VERSION_MINOR = 3
+VERSION_REV = 0
 VERSION = ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_REV}
 
 # minimum required zathura version
