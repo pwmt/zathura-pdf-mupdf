aeeb14fef50f9237f5cb86b56669e225b2e38def
Stardust <grasp-embark-spoof@duck.com>
2025-10-09 14:05:29 +0200

    Use fz_catch/fz_try when looping objects

--- zathura-pdf-mupdf/attachment.c
+++ zathura-pdf-mupdf/attachment.c
@@ -27,16 +27,24 @@ girara_list_t* pdf_document_attachments_get(zathura_document_t* document, void* 
 
   /* Extract attachments */
   g_mutex_lock(&mupdf_document->mutex);
-  pdf_document* pdf_doc = pdf_specifics(mupdf_document->ctx, mupdf_document->document);
-  pdf_filespec_params fs_params;
-  int n_objects  = pdf_xref_len(mupdf_document->ctx, pdf_doc);
+  fz_try(mupdf_document->ctx) {
+    pdf_document* pdf_doc = pdf_specifics(mupdf_document->ctx, mupdf_document->document);
+    pdf_filespec_params fs_params;
+    int n_objects  = pdf_xref_len(mupdf_document->ctx, pdf_doc);
 
-  for (int i = 1; i < n_objects; ++i) {
-    pdf_obj* obj = pdf_load_object(mupdf_document->ctx, pdf_doc, i);
-    if (pdf_is_embedded_file(mupdf_document->ctx, obj)) {
-      pdf_get_filespec_params(mupdf_document->ctx, obj, &fs_params);
-      girara_list_append(list, g_strdup(fs_params.filename));
+    for (int i = 1; i < n_objects; ++i) {
+      pdf_obj* obj = pdf_load_object(mupdf_document->ctx, pdf_doc, i);
+      if (pdf_is_embedded_file(mupdf_document->ctx, obj)) {
+        pdf_get_filespec_params(mupdf_document->ctx, obj, &fs_params);
+        girara_list_append(list, g_strdup(fs_params.filename));
+      }
     }
+  }
+  fz_catch(mupdf_document->ctx) {
+    if (error != NULL) {
+      *error = ZATHURA_ERROR_UNKNOWN;
+    }
+    goto error_free;
   }
   g_mutex_unlock(&mupdf_document->mutex);
 
@@ -61,19 +69,24 @@ zathura_error_t pdf_document_attachment_save(zathura_document_t* document, void*
   mupdf_document_t* mupdf_document = zathura_document_get_data(document);
 
   g_mutex_lock(&mupdf_document->mutex);
-  pdf_document* pdf_doc = pdf_specifics(mupdf_document->ctx, mupdf_document->document);
-  pdf_filespec_params fs_params;
-  int n_objects  = pdf_xref_len(mupdf_document->ctx, pdf_doc);
+  fz_try(mupdf_document->ctx) {
+    pdf_document* pdf_doc = pdf_specifics(mupdf_document->ctx, mupdf_document->document);
+    pdf_filespec_params fs_params;
+    int n_objects  = pdf_xref_len(mupdf_document->ctx, pdf_doc);
 
-  for (int i = 1; i < n_objects; ++i) {
-    pdf_obj* obj = pdf_load_object(mupdf_document->ctx, pdf_doc, i);
-    if (pdf_is_embedded_file(mupdf_document->ctx, obj)) {
-      pdf_get_filespec_params(mupdf_document->ctx, obj, &fs_params);
-      if (strcmp(fs_params.filename, attachmentname) != 0) {
-        continue;
+    for (int i = 1; i < n_objects; ++i) {
+      pdf_obj* obj = pdf_load_object(mupdf_document->ctx, pdf_doc, i);
+      if (pdf_is_embedded_file(mupdf_document->ctx, obj)) {
+        pdf_get_filespec_params(mupdf_document->ctx, obj, &fs_params);
+        if (strcmp(fs_params.filename, attachmentname) != 0) {
+          continue;
+        }
+        fz_save_buffer(mupdf_document->ctx, pdf_load_embedded_file_contents(mupdf_document->ctx, obj), file);
       }
-      fz_save_buffer(mupdf_document->ctx, pdf_load_embedded_file_contents(mupdf_document->ctx, obj), file);
     }
+  }
+  fz_catch(mupdf_document->ctx) {
+    return ZATHURA_ERROR_UNKNOWN;
   }
   g_mutex_unlock(&mupdf_document->mutex);
 
